2c93424bb7d3b739ae50c5ed86cf6295d3405578
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2022-12-14 14:43:03 +0700

    Update reader.c

--- r/reader.c
+++ r/reader.c
@@ -17,7 +17,6 @@ struct kampus{
 } prodi[jumlahProdi];
 
 int main(){
-    int i;
 	char *fname = "data.txt";
 	FILE *filename = fopen(fname, "r");
 	
@@ -36,10 +35,29 @@ int main(){
     printf("| Kode  |     Nama Prodi      | Status Prodi | Jenjang | Akreditasi | Jumlah Mahasiswa |\n");
     printf("+-------+---------------------+--------------+---------+------------+------------------+\n");
     
+    int i = 0;
+    float rata = 0.0;
+    
+    int max = 0, min = 0;
+    
     for(i=0; i<jumlahData; i++){
         printf("| %-5s | %-19s | %-12s | %-7s | %-10s | %-16d |\n", prodi[i].kode, prodi[i].nama, prodi[i].status, prodi[i].jenjang, prodi[i].akreditasi, prodi[i].jumlahMahasiswa);
         printf("+-------+---------------------+--------------+---------+------------+------------------+\n");
-    }
+    	rata += prodi[i].jumlahMahasiswa;
+    	
+    	if(prodi[max].jumlahMahasiswa < prodi[i].jumlahMahasiswa){
+    		max = i;
+		}
+		
+		if(prodi[max].jumlahMahasiswa > prodi[i].jumlahMahasiswa){
+    		min = i;
+		}
+	}
+    
+    printf("\nRata-rata jumlah mahasiswa UPN dalam 30 prodi di UPN adalah %.2f\n", (rata/jumlahData));
+
+    printf("\nProdi dengan mahasiswa paling banyak  : %-19s | Dengan jumlah mahasiswa : %d", prodi[max].nama, prodi[max].jumlahMahasiswa);
+    printf("\nProdi dengan mahasiswa paling sedikit : %-19s | Dengan jumlah mahasiswa : %d", prodi[min].nama, prodi[min].jumlahMahasiswa);
     
 	return 0;
 }

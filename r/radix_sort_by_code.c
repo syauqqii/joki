e3d81c1ecef5eb1ebe9f88f8e198c6260a5f6864
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2022-12-23 00:27:42 +0700

    Update radix_sort_by_code.c

--- r/radix_sort_by_code.c
+++ r/radix_sort_by_code.c
@@ -46,51 +46,52 @@ int getMin(int arr[], int n){
 	}
 }
 
-int get_max (long a[], int n){
-    int i, max = a[0];
-    
-    for (i = 1; i < n; i++)
-        if (a[i] > max)
-            max = a[i];
-            
-    return max;
+long get_Max(long a[], int n){
+    int i, max=a[0];
+    for(i=1;i<n;i++){
+        if(a[i]>max)
+            max=a[i];
+    }
+    return max;//maximumelementfromthearray
 }
-void radix_sort(long a[], int n){
-    int bucket[10][10], bucket_cnt[10];
-    int i, j, k, r, NOP = 0, divisor = 1, lar, pass;
-    lar = get_max(a, n);
-    while (lar > 0){
-        NOP++;
-        lar /= 10;
+
+void countingSort(long a[], int n, int place){
+    int i, output[n+1];
+    int count[10]={0};
+
+    //Calculatecountofelements
+    for(i=0;i<n;i++)
+    count[(a[i]/place)%10]++;
+
+    //Calculatecumulativefrequency
+    for(i=1;i<10;i++)
+        count[i]+=count[i-1];
+
+    //Placetheelementsinsortedorder
+    for(i=n-1;i>=0;i--){
+       output[count[(a[i]/place)%10]-1]=a[i];
+       count[(a[i]/place)%10]--;
     }
-    for (pass = 0; pass < NOP; pass++){
-        for (i = 0; i < 10; i++){
-            bucket_cnt[i] = 0;
-        }
-        
-        for (i = 0; i < n; i++){
-            r = (a[i] / divisor) % 10;
-            bucket[r][bucket_cnt[r]] = a[i];
-            bucket_cnt[r] += 1;
-        }
-        
-        i = 0;
-        
-        for (k = 0; k < 10; k++){
-            for (j = 0; j < bucket_cnt[k]; j++){
-                a[i] = bucket[k][j];
-                i++;
-            }
-        }
-        
-        divisor *= 10;
-    }
+
+    for(i=0;i<n;i++)
+        a[i]=output[i];
+}
+
+//functiontoimplementradixsort
+void radixsort(long a[], int n){
+    //getmaximumelementfromarray
+    int place, max=get_Max(a,n);
+
+    //Applycountingsorttosortelementsbasedonplacevalue
+    for(place=1;max/place>0;place*=10)
+    countingSort(a,n,place);
 }
 
 // main program
 int main(){
 	int arr[jumlahProdi];
 	long kode[jumlahProdi];
+	long temp[jumlahProdi];
 	
 	// initialization fname for file name
 	char *fname = "data.txt";
@@ -105,6 +106,7 @@ int main(){
 	// process read data with format, while next file line not null
 	while(!feof(filename)){
 		fscanf(filename, " %[^|]|%ld|%[^|]|%[^|]|%[^|]|%d", prodi[jumlahData].nama, &prodi[jumlahData].kode, prodi[jumlahData].status, prodi[jumlahData].jenjang, prodi[jumlahData].akreditasi, &prodi[jumlahData].jumlahMahasiswa);
+		temp[jumlahData] = prodi[jumlahData].kode;
 		jumlahData++;
 	}
 	
@@ -119,7 +121,7 @@ int main(){
 		kode[i] = prodi[i].kode;
 	}
 
-	radix_sort(kode, jumlahData);
+	radixsort(kode, jumlahData);
 
 	// make table header
     printf("+-------+---------------------+--------------+---------+------------+------------------+\n");
@@ -134,10 +136,11 @@ int main(){
     // looping to print all data needed
     for(i=0; i<jumlahData; i++){
         // print data
-		for(j=0; j<jumlahData; j++){
-			if(kode[i] == prodi[j].kode){
+		for(j=jumlahData-1; j>0; j--){
+			if(kode[i] == temp[j]){
 				printf("| %-5ld | %-19s | %-12s | %-7s | %-10s | %-16d |\n", prodi[j].kode, prodi[j].nama, prodi[j].status, prodi[j].jenjang, prodi[j].akreditasi, prodi[j].jumlahMahasiswa);
         		printf("+-------+---------------------+--------------+---------+------------+------------------+\n");
+				temp[j] = 0;
 			}
 		}
     	
